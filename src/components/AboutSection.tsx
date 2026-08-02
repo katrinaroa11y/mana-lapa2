0a0224a974d6a319ce92ee7cc41c1310aa5a6063
katrinaroa11y <katrina.rozenbaha@gmail.com>
2026-08-02 13:09:04 +0300

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -14,7 +14,7 @@ Mani veidojusi ir gan starptautiskā izglītība un pieredze Vācijā, gan miers
 
 Es ticu, ka ikvienā cilvēkā ir spēks un resurss pārvarēt grūtības, taču dažkārt to ir grūti saskatīt vienatnē. Mans mērķis ir radīt **drošu un atbalstošu vidi**, kurā kopā varam izprast notiekošo, atklāt stiprās puses un rast ceļu uz pārmaiņām.
 
-Savā darbā ievēroju **traumas informētu pieeju**. Tas nozīmē, ka izprotu psiholoģiskās traumas ietekmi uz cilvēka emocijām, domāšanu un uzvedību, kā arī pielāgoju procesu attiecīgi. Konsultācijās izmantoju **zinātniski pamatotas metodes**. Vairāk par izmantotajām metodēm un manu izglītību lasi [šeit](#Metodes-un-izglitiba).
+Savā darbā ievēroju **traumas informētu pieeju**. Tas nozīmē, ka izprotu psiholoģiskās traumas ietekmi uz cilvēka emocijām, domāšanu un uzvedību, kā arī pielāgoju procesu attiecīgi. Konsultācijās izmantoju **zinātniski pamatotas metodes**. Vairāk par izmantotajām metodēm un manu izglītību lasi sadaļā ''Metodes un izglītība''.
 
 **Mans mērķis ir palīdzēt Tev labāk izprast sevi, stiprināt psiholoģisko noturību un veidot dzīvi, kurā jūties līdzsvarotāk, pašpārliecinātāk un tuvāk sev.**`;
 
