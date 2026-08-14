8fdcdfee1900e11a1a2d959003393402e3e8b95c
katrinaroa11y <katrina.rozenbaha@gmail.com>
2026-08-14 17:21:30 +0300

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -12,7 +12,7 @@ const DEFAULT_ABOUT_TEXT = `**Mani sauc Katrīna Rozenbaha**, un es esmu **reģi
 
 **Es ticu, ka ikvienā cilvēkā ir spēks un resurss pārvarēt grūtības**, taču nereti to ir grūti saskatīt vienatnē. Mans mērķis ir radīt **drošu un atbalstošu vidi**, kurā atrast piemērotāko ceļu uz pārmaiņām.
 
-Līdzās profesionālām zināšanām būtiska ir cilvēcīga un empātiska attieksme pret katru klientu. Man ir svarīgi radīt drošu un pieņemošu vidi, kurā cilvēks var justies uzklausīts, saprasts un var brīvi runāt par sev svarīgo. Savā darbā apvienoju šo attieksmi ar zinātniski pamatotām metodēm, pielāgojot konsultāciju procesu katra cilvēka individuālajām vajadzībām un mērķiem..`;
+Līdzās profesionālām zināšanām būtiska ir cilvēcīga un empātiska attieksme pret katru klientu. Man ir svarīgi radīt drošu un pieņemošu vidi, kurā cilvēks var justies uzklausīts, saprasts un var brīvi runāt par sev svarīgo. Savā darbā apvienoju šo attieksmi ar zinātniski pamatotām metodēm, pielāgojot konsultāciju procesu katra cilvēka individuālajām vajadzībām un mērķiem.`;
 
 export const AboutSection: React.FC = () => {
   const [portraitSrc, setPortraitSrc] = useState(DEFAULT_PORTRAIT);
