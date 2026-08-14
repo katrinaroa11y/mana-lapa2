c1da2c6e97e587ae6af7d59d2cfdfa0063b8ad28
katrinaroa11y <katrina.rozenbaha@gmail.com>
2026-08-14 17:21:18 +0300

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -10,11 +10,9 @@ const TEXT_STORAGE_KEY = 'katrina_about_text_v99';
 
 const DEFAULT_ABOUT_TEXT = `**Mani sauc Katrīna Rozenbaha**, un es esmu **reģistrēta klīniskā psiholoģe**. Savā praksē esmu sniegusi atbalstu **vairāk nekā 1000 cilvēkiem**, konsultējot individuāli un vadot izglītojošas lekcijas par mentālo veselību un personīgo izaugsmi.
 
-Manu profesionālo pieeju veido daudzveidīga izglītība un darba pieredze Latvijā un Vācijā, kā arī bērnībā piedzīvotais miers un vienkāršība, uzaugot lauku viensētā Kurzemē. Šis pieredžu kopums man ļauj apvienot profesionālu pieeju ar cilvēcīgu un klātesošu attieksmi.
-
 **Es ticu, ka ikvienā cilvēkā ir spēks un resurss pārvarēt grūtības**, taču nereti to ir grūti saskatīt vienatnē. Mans mērķis ir radīt **drošu un atbalstošu vidi**, kurā atrast piemērotāko ceļu uz pārmaiņām.
 
-Savā darbā ievēroju **traumas informētu pieeju** un konsultācijās izmantoju **zinātniski pamatotas metodes**, kuras pielāgoju individuāli katra cilvēka vajadzībām un vēlmēm.`;
+Līdzās profesionālām zināšanām būtiska ir cilvēcīga un empātiska attieksme pret katru klientu. Man ir svarīgi radīt drošu un pieņemošu vidi, kurā cilvēks var justies uzklausīts, saprasts un var brīvi runāt par sev svarīgo. Savā darbā apvienoju šo attieksmi ar zinātniski pamatotām metodēm, pielāgojot konsultāciju procesu katra cilvēka individuālajām vajadzībām un mērķiem..`;
 
 export const AboutSection: React.FC = () => {
   const [portraitSrc, setPortraitSrc] = useState(DEFAULT_PORTRAIT);
